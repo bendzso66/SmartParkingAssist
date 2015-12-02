6a9f9802d956a28349a7eb94925247e73254413b
bendzso66 <bendzso66@gmail.com>
2015-12-03 00:02:46 +0100

    Use static readUrl method from AccessServlet class

--- app/src/main/java/hu/bme/hit/smartparkingassist/communication/FindFreeLotFromAddressTask.java
+++ app/src/main/java/hu/bme/hit/smartparkingassist/communication/FindFreeLotFromAddressTask.java
@@ -10,11 +10,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLEncoder;
 
 import hu.bme.hit.smartparkingassist.R;
@@ -36,7 +31,7 @@ public class FindFreeLotFromAddressTask extends AsyncTask<String, Void, String> 
             String address = params[0];
             String url = serverIpAddress+"findFreeLotFromAddress?address=Budapest+" + URLEncoder.encode(address, "UTF-8");
             Log.d("[Communicator]parameterezett url findFreelot-nal: ", url);
-            final String freeLots = readUrl(url);
+            final String freeLots = AccessServlet.readUrl(url);
             Log.d("[Communicator]findFreelotra servertol kapott valasz: ",freeLots);
 
             JSONArray jArray = null;
@@ -77,36 +72,5 @@ public class FindFreeLotFromAddressTask extends AsyncTask<String, Void, String> 
         Intent intent = new Intent(FIND_FREE_LOT_FROM_ADDRESS_FILTER);
         intent.putExtra(FIND_FREE_LOT_FROM_ADDRESS_KEY, result);
         LocalBroadcastManager.getInstance(ctx).sendBroadcast(intent);
-    }
-
-    public static String readUrl(String mapsApiDirectionsUrl) throws Exception {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            Log.d("[LotManager]:", mapsApiDirectionsUrl);
-            URL url = new URL(mapsApiDirectionsUrl);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.connect();
-            iStream = urlConnection.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    iStream));
-            StringBuffer sb = new StringBuffer();
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            data = sb.toString();
-            Log.d("[LotManager]:",data);
-            br.close();
-        } catch (Exception e) {
-            Log.d("Exception reading url", e.toString());
-            throw e;
-        } finally {
-            if(iStream != null)
-                iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
     }
 }
