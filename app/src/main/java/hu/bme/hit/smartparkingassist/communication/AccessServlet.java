3c778cb4c55bac0ff447cd6337ddc1a9466fc770
bendzso66 <bendzso66@gmail.com>
2015-12-02 23:53:19 +0100

    refactor AccessServlet class

--- app/src/main/java/hu/bme/hit/smartparkingassist/communication/AccessServlet.java
+++ app/src/main/java/hu/bme/hit/smartparkingassist/communication/AccessServlet.java
@@ -1,139 +1,21 @@
 package hu.bme.hit.smartparkingassist.communication;
 
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 public class AccessServlet {
 
-    String apiurl = "http://152.66.248.87:4567/"; //iparking.hit.bme.hu
-    //String apiurl = "http://192.168.1.4:4567/";
-    Activity display;
-    TextView view;
-    Button viewAllOnMapButton;
-
-    public AccessServlet(Activity iDisplay) {
-        display = iDisplay;
-    }
-
-    public AccessServlet(Activity iDisplay, TextView iView) {
-        display = iDisplay;
-        view = iView;
-    }
-
-    public AccessServlet(Activity iDisplay, TextView iView, Button iViewAllOnMapButton) {
-        display = iDisplay;
-        view = iView;
-        viewAllOnMapButton = iViewAllOnMapButton;
-    }
-
-    public boolean findFreeLotFromAddress(String address) {
-        //String url = apiurl+"findFreeLot?lat="+lat+"&lon="+lon+"&id="+id+"&rad="+walkdist;
-        String url = null;
-        try {
-            url = apiurl+"findFreeLotFromAddress?address=Budapest+" + URLEncoder.encode(address, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        new AsyncTask<Object, Void, Void>() {
-            private String url;
-
-            public AsyncTask<Object, Void, Void> setData(String curl) {
-                url = curl;
-
-                return this;
-            }
-
-            @Override
-            protected Void doInBackground(Object... param) {
-                try {
-                    Log.d("[Communicator]parameterezett url findFreelot-nal: ",url);
-                    final String ret = readUrl(url);
-                    Log.d("[Communicator]findFreelotra servertol kapott valasz: ",ret);
-
-                    JSONArray jArray = null;
-                    if(ret == null || ret.equalsIgnoreCase("[]") || ret.equalsIgnoreCase("UNSUCCESSFULL_REQUEST") ) {
-                        Log.d("mylog","Empty array, returning");
-                        display.runOnUiThread(new Runnable() {
-                            public void run() {
-                                Toast.makeText(display, "No Places Available", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                        return null;
-                    }
-                    try {
-                        jArray = new JSONArray(ret);
-                    } catch (JSONException e) {
-                        display.runOnUiThread(new Runnable() {
-                            public void run() {
-                                Toast.makeText(display, "Unexpected response from the server", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                        e.printStackTrace();
-                        return null;
-                    }
-
-                    String addresses = "";
-
-                    for (int i = 0; i < jArray.length(); i++) {
-                        try {
-                            JSONObject curr = jArray.getJSONObject(i);
-                            addresses += curr.getString("address") + "\n";
-                        } catch (JSONException e) {
-                            display.runOnUiThread(new Runnable() {
-                                public void run() {
-                                    Toast.makeText(display, "Unexpected response from the server", Toast.LENGTH_LONG).show();
-                                }
-                            });
-                            e.printStackTrace();
-                            return null;
-                        }
-                    }
-
-                    final String result = addresses;
-
-                    display.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(display, "Free lot was found.", Toast.LENGTH_LONG).show();
-                            view.setText(result);
-                            viewAllOnMapButton.setVisibility(TextView.VISIBLE);
-                        }
-                    });
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    display.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(display, "Free lot finder error.", Toast.LENGTH_LONG).show();
-
-                        }
-                    });
-                }
-
-                return null;
-            }
-        }.setData(url).execute();
-        return true; //new LatLng(lat, lon);
-    }
+    private static String data = "";
+    private static InputStream iStream = null;
+    private static HttpURLConnection urlConnection = null;
 
     public static String readUrl(String mapsApiDirectionsUrl) throws Exception {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
+
         try {
             Log.d("[LotManager]:", mapsApiDirectionsUrl);
             URL url = new URL(mapsApiDirectionsUrl);
