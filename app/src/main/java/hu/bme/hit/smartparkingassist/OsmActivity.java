318944aa7c588f76fea91ba7c3a052c02a2bd520
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-05-04 19:23:32 +0200

    Fix argument passing and return value in GetRoadPointsTask

--- app/src/main/java/hu/bme/hit/smartparkingassist/OsmActivity.java
+++ app/src/main/java/hu/bme/hit/smartparkingassist/OsmActivity.java
@@ -45,9 +45,13 @@ public class OsmActivity extends Activity {
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        new GetRoadPointsTask(this).execute();
-
         GeoPoint startPoint = new GeoPoint(47.4986276, 19.0700189);
+        GeoPoint endPoint = new GeoPoint(47.4971664, 19.070468);
+        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+        waypoints.add(startPoint);
+        waypoints.add(endPoint);
+        new GetRoadPointsTask(this).execute(waypoints);
+
         IMapController mapController = map.getController();
         mapController.setZoom(20);
         mapController.setCenter(startPoint);
@@ -73,7 +77,7 @@ public class OsmActivity extends Activity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(GetRoadPointsTask.GET_ROAD_POINTS_FILTER)) {
-                Road road = intent.getParcelableExtra(GetRoadPointsTask.GET_ROAD_POINTS_ROADS_KEY);
+                Road road = intent.getParcelableExtra(GetRoadPointsTask.GET_ROAD_POINTS_RESULT_KEY);
                 Polyline roadOverlay = RoadManager.buildRoadOverlay(road, Color.RED, 8, context);
                 map.getOverlays().add(roadOverlay);
                 map.invalidate();
--- app/src/main/java/hu/bme/hit/smartparkingassist/communication/GetRoadPointsTask.java
+++ app/src/main/java/hu/bme/hit/smartparkingassist/communication/GetRoadPointsTask.java
@@ -22,13 +22,11 @@ import java.util.ArrayList;
 import hu.bme.hit.smartparkingassist.R;
 import hu.bme.hit.smartparkingassist.items.WayItem;
 
-public class GetRoadPointsTask extends AsyncTask<String, Void, String> {
+public class GetRoadPointsTask extends AsyncTask<ArrayList<GeoPoint>, Void, Road> {
 
     public static final String GET_ROAD_POINTS_FILTER = "GET_ROAD_POINTS_FILTER";
     public static final String GET_ROAD_POINTS_RESULT_KEY = "GET_ROAD_POINTS_RESULT_KEY";
-    public static final String GET_ROAD_POINTS_ROADS_KEY = "GET_ROAD_POINTS_ROADS_KEY";
 
-    Road road;
     private Context ctx;
 
     public GetRoadPointsTask(Context ctx) {
@@ -36,27 +34,20 @@ public class GetRoadPointsTask extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected String doInBackground(String... params) {
-        GeoPoint startPoint = new GeoPoint(47.4986276, 19.0700189);
-        GeoPoint endPoint = new GeoPoint(47.4971664, 19.070468);
-        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-        waypoints.add(startPoint);
-        waypoints.add(endPoint);
-
+    protected Road doInBackground(ArrayList<GeoPoint>... params) {
+        ArrayList<GeoPoint> waypoints = params[0];
         RoadManager roadManager = new OSRMRoadManager(ctx);
-        road = roadManager.getRoad(waypoints);
+        Road road = roadManager.getRoad(waypoints);
 
-        return "Road points were found.";
+        return road;
     }
 
     @Override
-    protected void onPostExecute(String result) {
-        Log.d("[GetRoadPoints]", result);
-        Intent intent = new Intent(GET_ROAD_POINTS_FILTER);
-        intent.putExtra(GET_ROAD_POINTS_RESULT_KEY, result);
-        if (result.equals("Road points were found.")) {
-            intent.putExtra(GET_ROAD_POINTS_ROADS_KEY, road);
+    protected void onPostExecute(Road result) {
+        if (result != null) {
+            Intent intent = new Intent(GET_ROAD_POINTS_FILTER);
+            intent.putExtra(GET_ROAD_POINTS_RESULT_KEY, result);
+            LocalBroadcastManager.getInstance(ctx).sendBroadcast(intent);
         }
-        LocalBroadcastManager.getInstance(ctx).sendBroadcast(intent);
     }
 }
