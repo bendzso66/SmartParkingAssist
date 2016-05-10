cb557fcf24b987082144fc35cb16477049b79c97
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-05-10 19:42:44 +0200

    Send parking availability from SenParkingInfo activity too.

--- app/src/main/java/hu/bme/hit/smartparkingassist/GetParkingInfoActivity.java
+++ app/src/main/java/hu/bme/hit/smartparkingassist/GetParkingInfoActivity.java
@@ -1,9 +1,17 @@
 package hu.bme.hit.smartparkingassist;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,13 +23,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import hu.bme.hit.smartparkingassist.adapters.WayAdapter;
+import hu.bme.hit.smartparkingassist.communication.SendLotAvailabilityTask;
 import hu.bme.hit.smartparkingassist.communication.SendParkingConditionTask;
 import hu.bme.hit.smartparkingassist.communication.SendParkingLaneTask;
+import hu.bme.hit.smartparkingassist.service.LocationService;
+import hu.bme.hit.smartparkingassist.service.ObdService;
 
 public class GetParkingInfoActivity extends AppCompatActivity {
+
+    private Location currentLocation = null;
+    private Intent i = null;
+    private boolean isBound = false;
+    private LocationService myLocationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,5 +124,83 @@ public class GetParkingInfoActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        i = new Intent(getApplicationContext(), LocationService.class);
+        bindService(i, locationServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(LocationService.BR_NEW_LOCATION);
+        intentFilter.addAction(SendLotAvailabilityTask.SEND_FREE_LOT_FILTER);
+        intentFilter.addAction(ObdService.BR_PARKING_STATUS);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(
+                mMessageReceiver);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        unbindService(locationServiceConnection);
+        isBound = false;
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(LocationService.BR_NEW_LOCATION)) {
+                currentLocation = intent.getParcelableExtra(LocationService.KEY_LOCATION);
+                Log.d("[LOCATION] latitude: ", ((Double) currentLocation.getLatitude()).toString());
+                Log.d("[LOCATION] longitude: ", ((Double) currentLocation.getLongitude()).toString());
+                Log.d("[LOCATION] altitude: ", ((Double) currentLocation.getAltitude()).toString());
+                Log.d("[LOCATION] speed: ", ((Float) currentLocation.getSpeed()).toString());
+                Log.d("[LOCATION] provider: ", currentLocation.getProvider());
+                Log.d("[LOCATION] time: ", new Date(currentLocation.getTime()).toString());
+            } else if (intent.getAction().equals(SendLotAvailabilityTask.SEND_FREE_LOT_FILTER)) {
+                String result = intent.getStringExtra(SendLotAvailabilityTask.SEND_FREE_LOT_RESULT_KEY);
+                Snackbar.make(findViewById(android.R.id.content).getRootView(), result, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            } else if (intent.getAction().equals(ObdService.BR_PARKING_STATUS)) {
+                String availability = intent.getStringExtra(ObdService.PARKING_STATUS_KEY);
+                Log.d("[OBDService]", "Broadcast message is received: " + availability);
+
+                if (currentLocation != null) {
+                    Log.d("[SendFreeLot] GPS time: ", ((Long) currentLocation.getTime()).toString());
+                    Log.d("[SendFreeLot] current millis: ", ((Long) System.currentTimeMillis()).toString());
+                    if (currentLocation.getTime() + MainMenuActivity.THREE_MINUTE > System.currentTimeMillis()) {
+                        new SendLotAvailabilityTask(getApplicationContext()).execute(String.valueOf(currentLocation.getLatitude()),
+                                String.valueOf(currentLocation.getLongitude()),
+                                availability);
+                    }
+                }
+            }
+        }
+    };
+
+    private ServiceConnection locationServiceConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            LocationService.LocationBinder binder = (LocationService.LocationBinder) service;
+            myLocationService = binder.getService();
+            isBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+
+    };
 
 }
